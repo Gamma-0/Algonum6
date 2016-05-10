697a61a51acbc391fe8bb9eff604277eab61dcaf
DEHAIES DIT GERVAIS Nicolas <ndehaiesdit@ravens.pedago.ipb.fr>
2016-05-10 11:45:44 +0200

    Ajout fonction Euler

--- methode_num.py
+++ methode_num.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
+
 import numpy as np
 import numpy.linalg as lg
+
+# --- Méthodes numériques de résolution d'équations différentielles --- #
+
+# --- Méthode d'Euler --- #
+
+def euler(y, t, h, f):
+
+    res = y
+    k = 0
+
+    for i in range(0, y.shape[0]):
+        res[i] = y[i] + h * f(t[i], y[i])
+        
+    return res
