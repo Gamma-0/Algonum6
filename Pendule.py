64e22dd39c0fe7fc57689836e9308b0cae8140f3
Pravez <paul.ravezb@gmail.com>
2016-05-20 12:09:44 +0200

    Suite du pendule

--- Pendule.py
+++ Pendule.py
@@ -3,23 +3,23 @@
 #initialisation des variables
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 import classe
 
 g = 9.81                #acceleration de la pesanteur
 l = 1.0                 #longueur du pendule
-omega0 = np.sqrt(g/l)   #pulsation
+omega0 = 2*np.pi   #pulsation
 
 
 def equation_pendule(theta0, theta, omega0):
     return math.pow(theta0, 2)+2*math.pow(omega0, 2)*(math.cos(theta0) - math.cos(theta))
 
-#(application du principe fondamental de la dynamique)
-# on a deriv_theta = -g/l * sin(theta), mais comme on fait de petites oscillations, sin(theta) = theta
-def deriv_theta(theta):
-    return -(g/l)*theta
+def equadiff(X,t):
+    theta,theta_point=X
+    return np.array([theta_point, -g/l*np.sin(theta)])
 
-T0 = 2*math.pi*(math.sqrt(g/l))
 
-abscissas = [i for i in range(0, 1, 0.01)]
+theta0 = np.array([np.pi/2, 0])
 
-s = classe.pCauchy()
+s = classe.pCauchy(0.0, theta0, equadiff)
+res = s.aff_courbe_eq_diff(10, 0.0001)
