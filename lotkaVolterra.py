92b61975e6f90508edbc47da6af54922971a5f3b
xavdussieux <xavdussieux@enseirb-matmeca.fr>
2016-05-29 14:56:39 +0200

    lotka-volterra fini, commentaires faits, calcul periode OK

--- lotkaVolterra.py
+++ lotkaVolterra.py
@@ -7,8 +7,7 @@ from classe import *
 Resolution modele Malthus
 """
 def malthus():
-    """ La fonction "malthus" prend ... arguments :
-        - ... : ...
+    """ 
         Affiche la courbe de la variation de la population au cours du temps, en suivant le modèle de Malthus
     """
     t0 = 0.0
@@ -27,9 +26,8 @@ def malthus():
 Resolution modele Verhulst
 """
 def verhulst():
-    """ La fonction "verhulst" prend ... arguments :
-        - ... : ...
-        Affiche la courbe de la variation de la population au cours du temps, en suivant le modèle de Verhulst
+    """ 
+         Affiche la courbe de la variation de la population au cours du temps, en suivant le modèle de Verhulst
     """
     t0 = 0.0
     y0 = [5.0]
@@ -47,9 +45,12 @@ def verhulst():
 Resolution modele Lotka-Volterra
 """
 def lotka_volterra():
-    """ La fonction "lotka_volterra" prend ... arguments :
-        - ... : ...
-        Retourne ...
+    """ 
+        Simule le modele de lotka-volterra:
+           - varCouple.png : graphe des variations des proies et des prédateurs en fonction du temps
+           - affiche la période des solutions
+           - proie_predateur.png : graphe de l'evolution des predateurs en fonction des proies
+           - compLocal.png : graphe montrant le comportement local de plusieurs solutions
     """
     a = 2.0/3       # taux de reproduction des proies
     b = 4.0/3       # taux de mortalité des proies
@@ -61,8 +62,10 @@ def lotka_volterra():
     
     f_lv = lambda yt, t: np.array([yt[0] * (a - b * yt[1]), yt[1] * (c * yt[0] - d)])
     lv = pCauchy(t0, y0, f_lv)
-    #graph_couple(lv, tf)
-    #graph_proie_pred(lv, tf)
+    
+    graph_couple(lv, tf)
+    print("La periode des solution est : ", solution_period(lv, tf))
+    graph_proie_pred(lv, tf)
     graph_comportement_local(lv, 5.0)
 
 
@@ -144,8 +147,43 @@ def graph_comportement_local(equDiff, tf):
 
 
 
+def solution_period(equDiff, tf):
+    """ La fonction "solution_period" prend 2 arguments :
+        - equDiff : classe représentant le problème de Cauchy
+        - tf : borne max des abscisses
+        Retourne la periode des solutions
+    """
+    def extrem_loc(derivative):
+        """ La fonction "extrem_loc" prend 1 argument :
+           - derivative : tableau contenant les derivees
+           Retourne un tableau contenant les rangs des extremums
+        """        
+        extrem = []
+        last = derivative[0]
+        for i in range(1, np.shape(derivative)[0]):
+            if( (derivative[i] > 0 and last < 0) or # minimum local
+                (derivative[i] < 0 and last > 0) ): # maximum local
+                extrem.append(i)
+            last = derivative[i]
+        return extrem
+
+    sol = equDiff.meth_epsilon(tf, 10e-3, equDiff.step_euler)
+    size = np.shape(sol)[0]
+    t_sol = np.linspace(0, tf, size, endpoint=False)
+
+    #la periode de variation des proies est la meme que celle des predateurs
+    #prenons donc les proies pour le calcul
+    sol = sol[:,0]
+
+    #recherche d'extremum locaux
+    derivative = [(sol[i+1] - sol[i]) / (t_sol[i+1] - t_sol[i]) for i in range(size-1)]
+    extremum = extrem_loc(derivative)
+    
+    #la periode contient exactement 2 extremums locaux dans ce type de solution
+    period = 2 * ((t_sol[max(extremum)] - t_sol[min(extremum)]) / (len(extremum) - 1))
+    return period
+
 if __name__ ==  '__main__':
     lotka_volterra()
-    #malthus()
-    #verhulst()
-
+    malthus()
+    verhulst()
